7d5e00d8b720047d63a7db7ebcf1cccbf022c61a
catfather49 <catfather49@gmail.com>
2023-10-24 20:21:23 +0900

    first commit

---
+++ src/test/java/com/gogo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
